43881d6f794043db849ec04b5c8f2ce5fc90115f
Jorge Osses <jorgitoosses@gmail.com>
2022-07-17 21:59:54 -0300

    first commit

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,20 @@
+from distutils.command import upload
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imágen", upload_to="projects")
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actualización")
+
+
+class Meta:
+    verbose_name = "proyecto"
+    verbose_name_plural = "proyectos"
+    ordering = ["-created"]
